aea1500b04b7ba2a1d26e2a488777769b79d5593
zouhourhamza <zouhourhamzasimplonenda@gmail.com>
2021-04-26 20:44:16 +0100

    v2

--- src/component/carte.js
+++ src/component/carte.js
@@ -3,7 +3,7 @@ import {Card} from 'react-bootstrap';
 function Carte(props){
     return(
 <div>
-<div className="col-md-3 col-12 mx-auto"> 
+<div className=""> 
 <Card className="" border="light" text="light" style={props.style}>
    
     <Card.Body>
@@ -19,10 +19,31 @@ function Carte(props){
       </Card.Title>
       <Card.Text >
       <h5> {props.description}</h5> </Card.Text>
-     <div className="d-flex mt-5"> 
+     <div className="d-flex selection mt-5"> 
      
      
-     {props.selection}</div> 
+     
+     <select  name="country" id="country">
+    <option value="Choose a country" > Choose a country</option>
+    <option value="Argentina">Argentina</option>
+    <option value="Brazil">Brazil</option>
+    <option value="Chile">Chile</option>
+    <option value="Colombia">Colombia</option>
+    <option value="Itali">Itali</option>
+    <option value="Mexico">Mexico</option>
+    <option value="Spain">Spain</option>
+    <option value="Turquie">Turquie</option>
+    {props.selection}
+
+
+  </select>
+     
+     
+     
+     
+     
+     </div> 
+   
     </Card.Body>
   </Card>
   </div>
